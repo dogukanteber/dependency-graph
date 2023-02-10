0fde50f1cc412eb8313f44f95ee564bfb3125aba
Doğukan Teber <dogukanteber1@hotmail.com>
2023-02-10 18:13:03 +0300

    Add docstring to factory methods

--- dep_graph/test/factories.py
+++ dep_graph/test/factories.py
@@ -3,11 +3,27 @@ from ..graph import Graph, DependencyGraph
 
 class GraphFactory:
     @staticmethod
-    def build(data=None):
+    def build(data: dict = None) -> Graph:
+        """Creates a Graph object
+
+        Args:
+            data (dict, optional): Graph data as a dictionary. Defaults to None.
+
+        Returns:
+            Graph: created Graph object
+        """
         return Graph(data)
 
 
 class DependencyGraphFactory:
     @staticmethod
-    def build(graph):
+    def build(graph: Graph) -> DependencyGraph:
+        """Creates a DependencyGraph object from the given `Graph`
+
+        Args:
+            graph (Graph): Graph object
+
+        Returns:
+            DependencyGraph: created DependencyGraph object
+        """
         return DependencyGraph(graph)
